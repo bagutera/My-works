88a8baf4a0bd8601af61f6a315275a1f08c20847
Jevgeni Boikov <bagutera@gmail.com>
2017-12-06 14:11:20 +0200

    Create code_Test.php

---
+++ Converter_HTML_PHP/code_Test.php
@@ -0,0 +1,12 @@
+<?php
+include('code.php');
+if(changeCase('mari.liis','firstCapt')==="Mari.liis"){
+echo "Test is successfully completed.<br><br>";
+}else{echo "Test failed";}
+if(changeCase('mari.liis','commons')==="mari.liis"){
+echo "Test is successfully completed.<br><br>";
+}else{echo "Test failed";}
+if(changeCase('mari.liis','allCapts')==="MARI.LIIS"){
+echo "Test is successfully completed.<br><br>";
+}else{echo "Test failed";}
+?>
